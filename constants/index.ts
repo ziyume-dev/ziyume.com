1c9c1733d848087c38ec37c72a84f0c01f2ddfd1
besscroft <besscroft@foxmail.com>
2024-06-27 15:46:23 +0800

    first commit

---
+++ constants/index.ts
@@ -0,0 +1,2 @@
+export const appName = '紫梦工作室'
+export const appDescription = '希望能够通过编程，帮助他人和创造一些有意思的东西。'
---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div>
+    <h2>ziYume</h2>
+    <Footer />
+  </div>
+</template>
